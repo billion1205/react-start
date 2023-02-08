865ed7e24a2c52eb32bfe7c05169417f7c15593a
Eddy <pkw5282@naver.com>
2023-02-08 18:25:53 +0900

    일단완료

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -6,9 +6,7 @@ import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
 
-  const clickHanlder = () => {
-    setTitle("Update!");
-  };
+  
 
   return (
     <li>
@@ -18,7 +16,7 @@ function ExpenseItem(props) {
           <h2>{props.title}</h2>
           <div className="expense-item__price">{props.amount}</div>
         </div>
-        <button onClick={clickHanlder}>Change Title</button>
+     
       </Card>
     </li>
   );
